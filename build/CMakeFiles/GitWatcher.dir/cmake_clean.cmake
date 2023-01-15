b298efd7bf1e272f4e6ad1d14726ee1d4398bd87
haru864 <kawachigenji95@gmail.com>
2023-01-16 00:06:19 +0900

    first commit

---
+++ build/CMakeFiles/GitWatcher.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GitWatcher.dir/GitWatcher.c.o"
+  "CMakeFiles/GitWatcher.dir/GitWatcher.c.o.d"
+  "GitWatcher"
+  "GitWatcher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GitWatcher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/GitWatcher.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("searching for repo...\n");
+	return 0;
+}
